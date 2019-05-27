75370e635b49e88d60d0af5a789d9e27c46fd751
Shalinidea <Shalinidealmeida@gmail.com>
2019-05-27 20:45:26 +0800

    to-do without styling

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+
+const ul = document.querySelector("#todo-list");
+ul.addEventListener("click", function(event) {
+  if (event.target.className === "done") {
+    event.target.className = "";
+  } else {
+    event.target.className = "done";
+  }
+});
+
+function removeItem(event) {
+  const parent = event.target.parentElement;
+  parent.remove();
+}
+
+const button = document.querySelector("#add-to-do");
+button.addEventListener("click", function(event) {
+  const input = document.querySelector("input[type = 'text']");
+  let newItem = document.createElement("div");
+  newItem.innerHTML = `<li>${
+    input.value
+  }</li><button class='delete'> Delete</button >`;
+  ul.appendChild(newItem);
+  newItem.querySelector("button").addEventListener("click", removeItem);
+});
+
+const delbuttons = document.querySelectorAll(".delete");
+delbuttons.forEach(function(elem) {
+  elem.addEventListener("click", removeItem);
+});
